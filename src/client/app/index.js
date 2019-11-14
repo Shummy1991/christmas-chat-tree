1405809c9f58879e5ec67c3d10296b8be7c73baf
Daniel Bali <shummy1991@gmail.com>
2019-11-14 21:48:21 +0100

    update

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -46,7 +46,7 @@ const mapStateToProps = (state) => ({
 export default {
     component: withRouter(connect(mapStateToProps, { login, getUsers, startCommunication })(App)),
     loadData: async({ dispatch }) => {
-        console.log("load data app");
+        console.log("load data app", dispatch);
         await dispatch(login());
         await dispatch(getUsers());
     },
--- src/common/store/user/actions.js
+++ src/common/store/user/actions.js
@@ -1,7 +1,9 @@
 import { LOGIN } from "./types";
 
 export const login = (data) => async(dispatch, getState, api) => {
+    console.log("login");
     const { data: { success, user } } = await api.post("/login", data);
+
     if (success) {
         dispatch({
             type: LOGIN,
