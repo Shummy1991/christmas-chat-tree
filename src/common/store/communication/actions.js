5cccd0133cd3d74bc5a79961e8ec75cfd3842661
Bali Dániel <daniel.bali@benestudio.co>
2019-11-26 20:03:54 +0100

    task 1 entry point

--- src/common/store/communication/actions.js
+++ src/common/store/communication/actions.js
@@ -11,39 +11,20 @@ export const getUsers = () => async(dispatch, getState, api) => {
 // TASK 1: connection and getting data through SSE
 export const startCommunication = () => async(dispatch, getState, api) => {
     // connect to the eventstream @ baseURL + "/communication"
-    const eventSource = new EventSource(baseURL + "/communication");
+    // ...
     // listen for messages (you can send a message to see a new message arrive)
-    eventSource.onmessage = (message) => {
+    // ...
         // parse message.data
-        const { type, data } = JSON.parse(message.data);
-        switch (type) {
+        // ...
+        // switch
             // if message type is 'users' dispatch a GET_USERS type action with data as payload
-            // { type: '', payload: {} }
-            case "users": {
-                return dispatch({
-                    type: GET_USERS,
-                    payload: data,
-                });
-            }
+            // { type: ..., payload: ... }
+            // ...
             // if message type is message dispatch a RECEIVE_MESSAGE type action with data as paylaod
-            case "message": {
-                dispatch({
-                    type: RECEIVE_MESSAGE,
-                    payload: data,
-                });
+            // case ...
+                // ...
                 // after data.expiresIn ms dispatch a REMOVE_MESSAGE type action with data.id as payload 
-                setTimeout(() => {
-                    dispatch({
-                        type: REMOVE_MESSAGE,
-                        payload: data.id,
-                    });
-                }, data.expiresIn);
-                break;
-            }
-            default:
-                break;
-        }
-    };
+                // ...
 };
 
 export const sendMessage = data => async(dispatch, getState, api) => {
